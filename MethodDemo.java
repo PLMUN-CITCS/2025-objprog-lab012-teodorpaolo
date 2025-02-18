cbae2d3e2f8fef2bbd67a996457d10e9d1024ce4
pao <teodoropaoloalfonso_bsit@plmun.edu.ph>
2025-02-18 16:01:47 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,21 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+ greet(); // Calling the greet method
+  printMessage("This is my message!");
+}
+
+  public static void printMessage(String message) {
+ System.out.println(message);
+
+ }
+
+public static void greet() {
+
+  System.out.println("Hello from the greet method!");
+
+
+   }
+}
+
+
+
